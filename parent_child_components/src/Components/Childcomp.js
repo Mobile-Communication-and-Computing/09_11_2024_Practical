9caec6435e52a09136eef0466fab374e071a483b
piyumalBhathiya <139671996+piyumalBhathiya@users.noreply.github.com>
2024-11-21 17:24:07 +0000

    Add child component

--- parent_child_components/src/Components/Childcomp.js
+++ parent_child_components/src/Components/Childcomp.js
@@ -1,5 +1,10 @@
-export default function Childcomp(props){
-    return(
-        <></>
+export default function Childcomp(props) {
+    return (
+        <>
+            <h3>Hello React I'm Child</h3>
+            {
+                props.setfun("My Name is Bob")
+            }
+        </>
     );
 }
